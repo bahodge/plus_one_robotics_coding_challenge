974f5624ca5df4fd85651b3378cc6be643347a36
benhodge <benjamin.andrew.hodge@gmail.com>
2019-12-31 21:04:11 -0600

    working on the search result

--- src/components/search_results/SearchResult.js
+++ src/components/search_results/SearchResult.js
@@ -1,16 +1,61 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Grid, Header } from "semantic-ui-react";
+import { Grid, Header, List } from "semantic-ui-react";
 
 const SearchResult = props => {
   const { searchResult } = props;
   console.log("SearchResult Props", searchResult);
 
   if (searchResult) {
+    const {
+      Title,
+      Year,
+      Rated,
+      Released,
+      Runtime,
+      Genre,
+      Ratings,
+      Plot,
+      Poster,
+      Awards,
+      Language
+    } = searchResult;
+
     return (
       <Grid>
         <Grid.Row>
-          <Header as="h4">{searchResult.Title}</Header>
+          <Grid.Column>
+            <Header as="h5">Title: {Title}</Header>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row columns={2}>
+          <Grid.Column>
+            <strong>Release:</strong> {Released}
+          </Grid.Column>
+          <Grid.Column>
+            <strong>Runtime:</strong> {Runtime}
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <List>
+              {Ratings.map(({ Source, Value }, idx) => (
+                <List.Item key={idx}>
+                  <strong>{Source}</strong> - {Value}
+                </List.Item>
+              ))}
+            </List>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <p>{Plot}</p>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            {Poster ? <img src={Poster} alt={`${Title} Poster`} /> : null}}
+          </Grid.Column>
         </Grid.Row>
       </Grid>
     );
